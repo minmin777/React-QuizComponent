1008cb62c55c6ce91e0933bdc3eec3f9739c514a
xbbnxnh <yasmine.ayad@bnymellon.com>
2020-02-14 10:02:32 -0500

    QuizEnd

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component{
     render(){
-        return <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/></div>
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
+        return <div>
+            (isQuizEnd ? <QuizEnd /> :
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>)
+        </div>
     }
 
     constructor(props){
